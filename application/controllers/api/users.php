7876a4bf45299583fbbbba9e291d51b2017f2005
denieshnv <45711568+denieshnv@users.noreply.github.com>
2021-05-21 22:37:06 +0700

    GET API

--- application/controllers/api/users.php
+++ application/controllers/api/users.php
@@ -7,7 +7,29 @@ require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
 class Users extends REST_Controller{
-    
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Users_model','user');
+    }
+    public function index_get(){
+        $id = $this->get('id');
+        if ($id === null){
+            $users = $this->user->getUsers();
+        }else{
+            $users = $this->user->getUsers($id);
+        }
+        if ($users){
+            $this->response([
+                'status' => true,
+                'data' => $users
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'ID tidak ditemukan'
+            ], REST_Controller::HTTP_NOT_FOUND);  
+        }
+    }
 }
 
 ?>
